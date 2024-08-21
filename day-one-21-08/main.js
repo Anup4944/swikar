a7504e0b4d8b44180788d3f7a3bd9b96e6cf30c7
Anup <anuppoudel60@gmail.com>
2024-08-21 22:38:42 +1000

    day one resources

---
+++ day-one-21-08/main.js
@@ -0,0 +1,108 @@
+// **********
+
+// let a = 12;
+// let b = "ram";
+
+// let c = "some constant value";
+
+// console.log(c);
+
+// let loggedIn = false
+
+// true => true, "asddas", if not 0, {} , []
+// false => false, "" , 0, null, undefined
+
+// *********** OR and AND
+
+// const myName = "";
+// const name = myName || "ram";
+
+// this will give true
+
+// const isOld = false;
+// const name = isOld && 70;
+
+// Ternary Operator
+
+// ********** undefined, null n not defined
+
+// undefined
+
+// let a;
+
+// console.log(a);
+
+// let b;
+
+// console.log(b);
+
+// c;
+// console.log(c);
+
+// || ==> or operator, && ==> operator
+
+// LOOP
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// ********************* FUNCTIONS *********************
+
+// mun ko function define gareko
+// function hello(x) {
+//   console.log("hello", x);
+// }
+
+// hello("I am ex");
+
+// function hello(x) {
+//   return "the value is " + x;
+// }
+
+// const vale = hello(90); // yo chai function call gareko
+
+// const weekday = ["mon", "tue", "wed", "thru", "fri"];
+
+// const dropDown = weekday.map((i) => {
+//   return console.log(i);
+// });
+
+// const user = {
+//   name: "Anup",
+//   _id: 12,
+//   jobs: [
+//     {
+//       company: "some comp",
+//       salalry: 11000,
+//       hrs: 12,
+//     },
+//     {
+//       company: "some comp",
+//       salalry: 11000,
+//       hrs: 12,
+//     },
+//     {
+//       company: "some comp",
+//       salalry: 11000,
+//       activities: ["draw", "write", "clean"],
+//       hrs: 12,
+//     },
+//   ],
+// };
+
+// console.log(user.name);
+
+// DESTRUCTIING OBJECTS
+
+// const user = { name: "Anup", age: 26, country: "Nepal", isLiving: false };
+
+// const { name, age, country, isLiving } = user;
+
+// // DESTRUCTIING ARRAY
+
+// const jobs = ["manager", "developer", "tester"];
+
+// const [job1] = jobs;
+
+// console.log(job1);
