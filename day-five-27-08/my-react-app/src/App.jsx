f788a2d11cc9a35e95de5d95d2c1611bed5acd52
Anup <anuppoudel60@gmail.com>
2024-08-28 21:16:15 +1000

    day five

---
+++ day-five-27-08/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyButton from "./components/MyButton";
+
+function App() {
+  return (
+    <>
+      <MyButton title="Submit Data" />
+    </>
+  );
+}
+
+export default App;
---
+++ day-five-27-08/my-react-app/src/components/MyButton.jsx
@@ -0,0 +1,5 @@
+const MyButton = ({ title }) => {
+  return <div>{title}</div>;
+};
+
+export default MyButton;
